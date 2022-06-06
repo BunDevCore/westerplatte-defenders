63572f0983af1a3a1f422138a644b7427a39c647
LempekPL <superlempek2004@gmail.com>
2022-06-07 01:09:39 +0200

    defaults

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+// import AboutC from "/components/About"
+
+const About = () => {
+    return (
+        <>
+            {/*<AboutC/>*/}
+            <p>Wykonali Piotr Lempkowski i Wiktor Polanowski</p>
+        </>
+    )
+}
+export default About;
---
+++ pages/quiz.jsx
@@ -0,0 +1,11 @@
+// import QuizC from "/components/Quiz"
+
+const Quiz = () => {
+    return (
+        <>
+            {/*<QuizC/>*/}
+            <p>TODO</p>
+        </>
+    )
+}
+export default Quiz;
