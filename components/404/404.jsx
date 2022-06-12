d1710f0e7052d77643e22ba2a4bb9acdc46d7269
LempekPL <superlempek2004@gmail.com>
2022-06-12 02:32:06 +0200

    creating files

---
+++ components/404/404.jsx
@@ -0,0 +1,11 @@
+import useTranslation from "next-translate/useTranslation";
+
+const C404 = () => {
+    const {t} = useTranslation("common");
+
+    return (
+        <>
+        </>
+    );
+}
+export default C404;
