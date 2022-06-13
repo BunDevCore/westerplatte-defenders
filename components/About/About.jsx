060d460d8fd1fb63b639d9ee201b87c9e526539d
LempekPL <superlempek2004@gmail.com>
2022-06-13 22:53:43 +0200

    full about

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,16 +1,30 @@
-import {Carrd, Github, Main, Name} from "./about.style";
+import {
+    AboutProject,
+    Carrd,
+    CarrdBox,
+    CarrdImage,
+    FocusStyledA,
+    Main,
+    Name, PLWarn,
+    Section,
+    UserLink
+} from "./about.style";
 import Link from "next/link";
+import Image from "next/image";
+import useTranslation from "next-translate/useTranslation";
 
 const About = () => {
+    const {t, lang} = useTranslation("about");
+
     const authors = [
         {
-            imageSRC: "/images/lempek",
+            imageSRC: "/images/lempek.jpg",
             name: "Piotr „Lempek” Lempkowski",
             github: "https://github.com/LempekPL",
             twitter: "https://twitter.com/LEMPEKPL"
         },
         {
-            imageSRC: "/images/wikapo",
+            imageSRC: "/images/wikapo.jpg",
             name: "Wiktor „wiKapo” Polanowski",
             github: "https://github.com/wiKapo"
         }
@@ -26,16 +40,56 @@ const About = () => {
 
     for (const author of authors) {
         cards.push(<Carrd key={author.name}>
+            <CarrdImage>
+                <Image src={author.imageSRC} alt={author.name} width={"192"} height={"192"}/>
+            </CarrdImage>
             <Name>{author.name}</Name>
-            <Link href={author.github} passHref>
-                <Github>{author.github}</Github>
-            </Link>
+            {author?.github && <UserLink>
+                <Link href={author.github} passHref>
+                    <FocusStyledA>
+                        Github
+                    </FocusStyledA>
+                </Link>
+            </UserLink>}
+            {author?.twitter && <UserLink>
+                <Link href={author.twitter} passHref>
+                    <FocusStyledA>
+                        Twitter
+                    </FocusStyledA>
+                </Link>
+            </UserLink>}
         </Carrd>);
+    }
+
+    let sourcesRendered = [];
+
+    for (const source of sources) {
+        sourcesRendered.push(
+            <li>
+                <Link href={source} passHref>
+                    <FocusStyledA>
+                        {source}
+                    </FocusStyledA>
+                </Link>
+            </li>
+        )
     }
 
     return (<>
         <Main>
-            {cards}
+            <Section>Autorzy</Section>
+            <CarrdBox>
+                {cards}
+            </CarrdBox>
+            <Section>O projekcie</Section>
+            <AboutProject>
+                {t("about")}
+            </AboutProject>
+            <Section>Źródła</Section>
+            {lang === "en" && <PLWarn>Sources are in Polish!!!</PLWarn>}
+            <ul>
+                {sourcesRendered}
+            </ul>
         </Main>
     </>);
 }
--- components/About/about.style.js
+++ components/About/about.style.js
@@ -7,21 +7,97 @@ export const Main = styled.main`
   border: 2px solid ${props => props.theme.main_color};
   color: ${props => props.theme.main_color};
   border-radius: 2rem;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
 
+  @media (max-width: 1500px) {
+    margin-left: 10rem;
+    margin-right: 10rem;
+  }
+
+  @media (max-width: 800px) {
+    margin: calc(var(--navbar-height)) 2rem 0;
+    border-radius: 0;
+    border: 2px solid transparent;
+  }
+
+  @media (max-width: 500px) {
+    margin-left: 0;
+    margin-right: 0;
+  }
+`;
+
+export const CarrdBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 1rem;
+  gap: 1rem;
+  
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
 `;
 
 export const Carrd = styled.div`
+  background-color: ${props => props.theme.E404_background};
+  border: 2px solid ${props => props.theme.main_color};
+  color: ${props => props.theme.main_color};
+  border-radius: 2rem;
+  width: 20rem;
+  padding: 1rem;
+  margin: 0 auto;
 
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+`;
+
+export const Section = styled.p`
+  text-align: center;
+  font-weight: bold;
+  font-size: 2rem;
 `;
 
 export const Name = styled.p`
-
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.25rem;
 `;
 
-export const Github = styled.a`
+export const UserLink = styled.p`
+  text-align: center;
+  margin-top: .5rem;
+  font-size: 1.15rem;
+  color: #003bb4;
+`;
 
+export const FocusStyledA = styled.a`
+  color: #003bb4;
+  
+  :is(:hover, :focus-visible) {
+    text-decoration: underline;
+  }
+  
+  :focus-visible {
+    outline: 2px solid royalblue;
+  }
 `;
 
 export const CarrdImage = styled.div`
+  padding: 0 2.5rem;
 
+  span {
+    border-radius: 100%;
+    border: 2px solid ${props => props.theme.main_color} !important;
+  }
+`;
+
+export const AboutProject = styled.p`
+  padding: 1rem;
+`;
+
+export const PLWarn = styled.p`
+  padding-left: 1rem;
+  color: #ff8000;
 `;
