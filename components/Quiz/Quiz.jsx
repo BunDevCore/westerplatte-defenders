96518ae1de29f63b1717413ca2764cda8bcbe7e4
LempekPL <superlempek2004@gmail.com>
2022-06-14 01:34:19 +0200

    full quiz

--- components/Quiz/Quiz.jsx
+++ components/Quiz/Quiz.jsx
@@ -1,9 +1,93 @@
-
+import {
+    AnswerBox,
+    AnswerButton,
+    Congratulations,
+    EndButton,
+    Main,
+    MainQuestion,
+    Score,
+    StartButton
+} from "./quiz.style";
+import useTranslation from "next-translate/useTranslation";
+import {useState} from "react";
 
 const Quiz = () => {
-    return(<>
+    const {t} = useTranslation("quiz")
+    let [question, setQuestion] = useState(null);
+    let used = [];
+    let [currQ, setCurrQ] = useState(0);
+    let [points, setPoints] = useState(0);
+    const questionList = t("questions", {count: 0}, {returnObjects: true});
+    let good = -1;
 
+    const start = () => {
+        used = [];
+        currQ = 0;
+        changeQuestion();
+    }
+
+    const changeQuestion = () => {
+        setCurrQ(currQ++);
+        let random;
+        if (used.length >= 18) {
+            setQuestion(<>
+                <Congratulations>Odpowiedziałeś na wszystkie pytania</Congratulations>
+                <Score>Twój wynik {points}/{questionList.length}</Score>
+                <EndButton onClick={() => {
+                    start()
+                }}>ZAGRAJ JESZCZE RAZ</EndButton>
+            </>);
+        } else {
+            do {
+                random = Math.floor(Math.random() * questionList.length)
+            } while (used.includes(random));
+            used.push(random)
+            let answers = [];
+            let i = 0;
+            for (const wrong of questionList[random].wrong) {
+                let j = i;
+                answers.push(<AnswerButton onClick={() => checkQuestion(j)} key={i}>{wrong}</AnswerButton>);
+                i++;
+            }
+            good = i;
+            answers.push(
+                <AnswerButton onClick={() => checkQuestion(i)} key={i}>{questionList[random].good}</AnswerButton>
+            );
+            console.log(answers);
+            setQuestion(<>
+                <MainQuestion>{questionList[random].question} {currQ}/{questionList.length}</MainQuestion>
+                <AnswerBox>
+                    {shuffle(answers)}
+                </AnswerBox>
+            </>);
+        }
+    }
+
+    const checkQuestion = (answer) => {
+        if (answer === good) {
+            setPoints(points++);
+        }
+        changeQuestion();
+    }
+
+    return (<>
+        <Main>
+            {question == null && <StartButton onClick={() => {
+                start()
+            }}>START QUIZ</StartButton>}
+            {question}
+        </Main>
     </>);
+}
+
+const shuffle = (array) => {
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
 }
 
 export default Quiz;
--- components/Quiz/quiz.style.js
+++ components/Quiz/quiz.style.js
@@ -1,1 +1,98 @@
 import styled from "styled-components";
+
+export const Main = styled.main`
+  margin: calc(var(--navbar-height) + 1rem) 30rem auto;
+  background-color: ${props => props.theme.E404_background};
+  border: 2px solid ${props => props.theme.main_color};
+  color: ${props => props.theme.main_color};
+  border-radius: 2rem;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+
+  @media (max-width: 1500px) {
+    margin-left: 10rem;
+    margin-right: 10rem;
+  }
+
+  @media (max-width: 800px) {
+    margin: calc(var(--navbar-height)) 2rem 0;
+    border-radius: 0;
+    border: 2px solid transparent;
+  }
+
+  @media (max-width: 500px) {
+    margin-left: 0;
+    margin-right: 0;
+  }
+`;
+
+export const StartButton = styled.button`
+  border: 2px solid ${props => props.theme.main_color};
+  color: ${props => props.theme.E404_button_text};
+  background-color: ${props => props.theme.E404_button};
+  border-radius: 1rem;
+  cursor: pointer;
+  padding: 2rem 0;
+  margin: 0 auto;
+  width: 50%;
+  font-size: clamp(1rem, 1vw, 20rem);
+  transition-duration: .3s;
+
+  :focus-visible {
+    outline: 2px solid royalblue;
+  }
+
+  :is(:focus-visible, :hover) {
+    color: #fff;
+    background-color: ${props => props.theme.main_color};
+  }
+`;
+
+export const EndButton = styled(StartButton)`
+  margin-top: 2rem;
+`;
+
+export const AnswerButton = styled.button`
+  border: 2px solid ${props => props.theme.main_color};
+  color: ${props => props.theme.E404_button_text};
+  background-color: ${props => props.theme.E404_button};
+  border-radius: 1rem;
+  cursor: pointer;
+  margin: 1rem;
+  padding: 1rem;
+  width: 50%;
+  font-size: clamp(1rem, 1vw, 20rem);
+  transition-duration: .3s;
+  align-self: center;
+
+  :focus-visible {
+    outline: 2px solid royalblue;
+  }
+
+  :is(:focus-visible, :hover) {
+    color: #fff;
+    background-color: ${props => props.theme.main_color};
+  }
+`;
+
+export const MainQuestion = styled.p`
+  margin: 1rem;
+  font-weight: bold;
+  font-size: 2rem;
+`;
+
+export const Congratulations = styled(MainQuestion)`
+  text-align: center;
+`;
+
+export const AnswerBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 2;
+`;
+
+export const Score = styled.p`
+  font-size: 2rem;
+  text-align: center;
+`;
